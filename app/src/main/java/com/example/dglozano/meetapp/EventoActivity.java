cc70c688961d15326578b0a6d75241694939b550
Diego Garcia Lozano <diegogarcialozano95@gmail.com>
2018-02-02 16:50:55 -0300

    Crea Toolbar para la activity Evento
    Falta cambiar titulos en los fragmentos y darle funcionalidad a la felchita
    de atras, el buscar y el menu.

--- app/src/main/java/com/example/dglozano/meetapp/EventoActivity.java
+++ app/src/main/java/com/example/dglozano/meetapp/EventoActivity.java
@@ -1,6 +1,8 @@
 package com.example.dglozano.meetapp;
 
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
 import android.support.v4.app.Fragment;
@@ -8,6 +10,10 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.dglozano.meetapp.fragments.DivisionGastosPageFragment;
 import com.example.dglozano.meetapp.fragments.ParticipantesPageFragment;
@@ -39,6 +45,14 @@ public class EventoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evento);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
@@ -54,6 +68,39 @@ public class EventoActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabDotsTareasParticipantesGastos);
         tabLayout.setupWithViewPager(mViewPager, true);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                System.out.println("Hizo click en settings");
+                return true;
+
+            case R.id.toolbar_search:
+                // User chose the Search option.
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                System.out.println("Hizo click en buscar");
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_evento, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.toolbar_search);
+        SearchView searchView =
+                (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        // TODO: Configure the search info and add any event listeners...
+        return super.onCreateOptionsMenu(menu);
+    }
+
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
